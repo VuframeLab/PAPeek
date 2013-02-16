1afbe00fcaa2bee53d70eb3292eb4256565705d4
Daniel Dengler <ddengler@dopanic.com>
2013-02-16 19:08:54 +0100

    add: operation queue and removal of unused images

--- Classes/PAPeek.m
+++ Classes/PAPeek.m
@@ -11,6 +11,7 @@
 
 @interface PAPeek () {
 	UIScrollView *scrollView;
+    NSOperationQueue *queue;
 //    MBProgressHUD *_progressHUD;
 }
 
@@ -44,6 +45,7 @@
     scrollView.userInteractionEnabled = YES;
     scrollView.scrollsToTop = YES;
     scrollView.backgroundColor = [UIColor blackColor];
+    scrollView.delegate = self;
     self.wantsFullScreenLayout = YES;
     self.view = scrollView;
     
@@ -52,6 +54,9 @@
                                                           action:@selector(handleSingleTap:)];
     [singleTapGestureRecognizer setNumberOfTapsRequired:1];
     [self.view addGestureRecognizer:singleTapGestureRecognizer];
+    
+    queue = [NSOperationQueue new];
+    queue.maxConcurrentOperationCount = 1;
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -61,16 +66,53 @@
     UIImageView *imageView;
     
     for (int i = 0; i < [self numberOfImages]; i++) {
-        imageView = [[UIImageView alloc] initWithImage:[self imageAtIndex:i]];
+        imageView = [[UIImageView alloc] init];
         imageView.frame = CGRectMake(scrollView.frame.size.width * i, 0, scrollView.frame.size.width, scrollView.frame.size.height);
         imageView.contentMode = UIViewContentModeScaleAspectFit;
         imageView.userInteractionEnabled = YES;
         [scrollView addSubview:imageView];
     }
     
+    [self queueLoadImageForViewAtIndex:[self index]];
+    
+    if ([self index] > 0) {
+        [self queueLoadImageForViewAtIndex:[self index] - 1];
+    }
+    
+    if ([self index] < [self numberOfImages] - 1) {
+        [self queueLoadImageForViewAtIndex:[self index] + 1];
+    }
+    
     scrollView.contentSize = CGSizeMake(scrollView.frame.size.width * [self numberOfImages], scrollView.frame.size.height);
     
     [scrollView scrollRectToVisible:CGRectMake(scrollView.frame.size.width * [self index], 0, scrollView.frame.size.width, scrollView.frame.size.height) animated:NO];
+}
+
+- (void)queueLoadImageForViewAtIndex:(NSUInteger)index{
+    NSInvocationOperation *operation;
+    operation = [[NSInvocationOperation alloc] initWithTarget:self
+                                                     selector:@selector(loadImageForViewAtIndex:)
+                                                       object:[[NSNumber alloc] initWithInt:index]];
+    [queue addOperation:operation];
+}
+
+- (void)loadImageForViewAtIndex:(NSNumber *)index {
+    // TODO: replace unsafe UIImage use with CGImage
+    UIImageView *imageView = (UIImageView*)[scrollView.subviews objectAtIndex:[index integerValue]];
+    if(imageView.image == nil) {
+        UIImage *image = [self imageAtIndex:[index integerValue]];
+        [imageView performSelectorOnMainThread:@selector(setImage:) withObject:image waitUntilDone:YES];
+    }
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    // load first image
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    [queue cancelAllOperations];
 }
 
 - (void)didReceiveMemoryWarning
@@ -103,9 +145,33 @@
 
 #pragma mark - UIScrollViewDelegate
 
-//- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
-//{
-//    return imageView;
-//}
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)currentScrollView
+{
+    // TODO: remove hack of scroll view width and react to orientation change
+    int index = floor(currentScrollView.contentOffset.x / [[UIScreen mainScreen] bounds].size.height);
+    
+    // just in case
+    UIImageView *imageView;
+    for (int i = 0; i < [self numberOfImages]; i++) {
+        imageView = (UIImageView*)[[scrollView subviews] objectAtIndex:i];
+        if (i < index - 1){
+            imageView.image = nil;
+        } else if (i > index + 1) {
+            imageView.image = nil;
+        }
+    }
+    [queue cancelAllOperations];
+    [self queueLoadImageForViewAtIndex:index];
+    
+    if (index > 0) {
+        [self queueLoadImageForViewAtIndex:index - 1];
+    }
+    
+    if (index < [self numberOfImages] - 1) {
+        [self queueLoadImageForViewAtIndex:index + 1];
+    }
+    
+
+}
 
 @end
--- PAPeek.podspec
+++ PAPeek.podspec
@@ -1,11 +1,11 @@
 Pod::Spec.new do |s|
   s.name     = 'PAPeek'
-  s.version  = '0.1'
+  s.version  = '0.2'
   s.license  = 'MIT'
   s.summary  = 'A simple universal photo browser'
   s.homepage = 'https://github.com/doPanic/PAPeek'
   s.author   = { 'Daniel Dengler' => 'ddengler@dopanic.com' }
-  s.source   = { :git => 'https://github.com/doPanic/PAPeek.git', :tag => '0.1' }
+  s.source   = { :git => 'https://github.com/doPanic/PAPeek.git', :tag => '0.2' }
   s.platform = :ios
   s.requires_arc = true
    
